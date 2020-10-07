f164c60aba7b24ca7076f7d8d27d6d1582e3d0c0
tyugv <milsocol@gmail.com>
2020-10-07 12:03:20 +0300

    from 0

--- mainWindow.py
+++ mainWindow.py
@@ -2416,8 +2416,8 @@ class Ui_MainWindow(object):
         self.pushButton.setText(_translate("MainWindow", "Выберите файл"))
         self.label.setText(_translate("MainWindow", "Файл не был выбран"))
         self.sinButton.setText(_translate("MainWindow", "Фиксировать элемент"))
-        self.goriz_label.setText(_translate("MainWindow", "1    2    3    4    5   6   7   8   9   10  11  12 13  14  15 16  17 18"))
-        self.vertic_label.setText(_translate("MainWindow", "1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18"))
+        self.goriz_label.setText(_translate("MainWindow", "0    1    2    3    4   5    6   7   8   9  10  11  12  13  14 15  16 17"))
+        self.vertic_label.setText(_translate("MainWindow", "0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17"))
 
     # показать общие графики
     def show_common_pics(self):
